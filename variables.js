143a6f9ff31923c7ff3459cfd5ecdc22d24e9c7e
s8or <s8or@fresnou.org>
2023-02-06 10:21:52 -0800

    initial commit

---
+++ variables.js
@@ -0,0 +1,81 @@
+// variables store data for later use
+
+//create a variable with let
+let myvariable ="vaule";
+let mynewvariable = 7;
+let mybool = true;
+
+//accessing variable values
+console.log(myvariable);
+console.log(mynewvariable+5);
+console.log(mybool && true);
+
+//update value stored in a variable
+mynewvariable = 10;
+console.log(mynewvariable);
+
+//increase by 1
+mynewvariable=mynewvariable+1;
+console.log(mynewvariable);
+
+//multiply by 3
+mynewvariable = mynewvariable * 3;
+console.log(mynewvariable);
+
+//divide by 3
+mynewvariable = mynewvariable / 3;
+console.log(mynewvariable);
+
+//divide and return the remainder
+mynewvariable = mynewvariable % 2; // 10/2 = 5 with 0 remainder
+console.log(mynewvariable);
+
+// Shortcuts!
+
+let x = 100;
+
+x = x+1; //add 1
+
+x++; //add 1
+
+
+
+x = x - 1; //subtract 1
+
+x--; //subtract 1
+
+
+
+x = x + 5; //add 5
+
+x+= 5; //add 5
+
+
+
+x = x - 2; //subtract 2
+
+x -= 2; //subtract 2
+
+
+
+x = x * 10; //times 10
+
+x *= 10; //times 10
+
+
+
+x = x / 10; //divide by 10
+
+x /= 10; //divide by 10
+
+// Keywords
+
+const pi = 3.14;
+
+const userName = "myUser123";
+
+// constants cannot change values
+
+var old = 123;
+
+// var is the old way to create variables. Don't use it.
